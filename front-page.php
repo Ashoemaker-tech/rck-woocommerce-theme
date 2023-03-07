b944fafbe8ee2afdde4adb32bce4e90dfceada8d
Ashoemaker-tech <andrew.shoemaker9@gmail.com>
2023-03-06 21:21:51 -0600

    finished front-page

--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -16,43 +17,89 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-<section
-  class="relative bg-[url(https://images.pexels.com/photos/5973854/pexels-photo-5973854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)] bg-cover bg-center bg-no-repeat"
->
-  <div
-    class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"
-  ></div>
+  <section class="relative  bg-cover bg-center bg-no-repeat" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/hero-bg.webp');">
+    <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"></div>
 
-  <div
-    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
-  >
-    <div class="max-w-xl text-center sm:text-left">
-      <h1 class="text-3xl font-extrabold sm:text-5xl">
-        Precision is the key to
+    <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+      <div class="max-w-xl text-center sm:text-left">
+        <h1 class="text-3xl font-extrabold sm:text-5xl">
+          Precision is the key to
 
-        <strong class="block font-extrabold text-rose-700">
-          our success.
-        </strong>
-      </h1>
+          <strong class="block font-extrabold text-rose-700">
+            our success.
+          </strong>
+        </h1>
 
-      <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-        tenetur fuga ducimus numquam ea!
-      </p>
+        <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+          tenetur fuga ducimus numquam ea!
+        </p>
 
-      <div class="mt-8 flex flex-wrap gap-4 text-center">
-        <a
-          href="/shop"
-          class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow text-black hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
-        >
-          Shop Now
-        </a>
+        <div class="mt-8 flex flex-wrap gap-4 text-center">
+          <a href="/shop" class="block shop-btn w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow text-black hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto">
+            Shop Now
+          </a>
 
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+
+  <section class="container mx-auto mt-24">
+    <header class="text-center">
+      <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Popular Products</h2>
+      <p class="max-w-md mx-auto mt-4 text-gray-500">People can't get enough of these items.</p>
+    </header>
+    <div class="my-8">
+      <?= do_shortcode('[products colums=4 limit=4]'); ?>
+    </div>
+  </section>
+
+  <section class="container mx-auto mt-24">
+    <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+      <header class="text-center">
+        <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Categories</h2>
+        <p class="max-w-md mx-auto mt-4 text-gray-500">Our most popular categories.</p>
+      </header>
+      <ul class="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+        <li>
+          <a href="/home-decor" class="relative block group">
+            <img src="<?= get_template_directory_uri(); ?>/img/home-decor.jpg" alt="" loading="lazy" class="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90" />
+            <div class="absolute inset-0 flex flex-col items-start justify-end p-6">
+              <h3 class="text-xl font-medium text-white">Home Decor</h3>
+              <span class="mt-1.5 inline-block bg-rose-600 rounded px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                Shop Now
+              </span>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="/kitchen" class="relative block group">
+            <img src="<?= get_template_directory_uri(); ?>/img/kitchen.jpg" alt="" loading="lazy" class="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90" />
+            <div class="absolute inset-0 flex flex-col items-start justify-end p-6">
+              <h3 class="text-xl font-medium text-white">Kitchen</h3>
+              <span class="mt-1.5 inline-block bg-rose-600 rounded px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                Shop Now
+              </span>
+            </div>
+          </a>
+        </li>
+        <li class="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+          <a href="/furniture" class="relative block group">
+            <img src="<?= get_template_directory_uri(); ?>/img/custom.jpg" alt="" loading="lazy" class="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90" />
+            <div class="absolute inset-0 flex flex-col items-start justify-end p-6">
+              <h3 class="text-xl font-medium text-white">Custom Furniture</h3>
+              <span class="mt-1.5 inline-block bg-rose-600 rounded px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
+                Shop Now
+              </span>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
 </main><!-- #main -->
 
 <?php
-// get_footer();
+get_footer();
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,120 +13,77 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-<header aria-label="Site Header" class="border-b border-gray-100">
-  <div
-    class="mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8"
-  >
-    <div class="flex items-center">
-<!-- mobile dropdown -->
-<div x-data="{ isActive: false }" class="relative">
-  <div
-    class="inline-flex items-center overflow-hidden bg-white"
-  >
-    <button x-on:click="isActive = !isActive" type="button" class="p-2 sm:mr-4 lg:hidden border-none">
-            <span class="sr-only">Menu</span>
-        <svg
-          class="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      </button>
-  </div>
+  <?php wp_body_open(); ?>
+  <div id="page" class="site">
+    <header id="Top" aria-label="Site Header" class="border-b border-gray-100">
+      <div class="mx-auto flex h-20 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8">
+        <div class="flex items-center">
+          <!-- mobile dropdown -->
+          <div x-data="{ isActive: false }" class="relative">
+            <div class="inline-flex items-center overflow-hidden bg-white">
+              <button x-on:click="isActive = !isActive" type="button" class="p-2 sm:mr-4 lg:hidden border-none">
+                <span class="sr-only">Menu</span>
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
+            </div>
 
-  <div
-    class="lg:hidden absolute left-0 z-10 mt-2 w-56 rounded-md border border-gray-100 bg-white shadow-lg"
-    role="menu"
-    x-cloak
-    x-transition
-    x-show="isActive"
-    x-on:click.away="isActive = false"
-    x-on:keydown.escape.window="isActive = false"
-  >
-    <div class="p-2 mobile-menu">
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			)
-		);
-		?>
-    </div>
-  </div>
-</div>
-		<!-- /Mobile dropdown -->
-        <span class="sr-only">Logo</span>
-		<span class="inline-block h-16 w-16">
-			<?php the_custom_logo(); ?>
-		</span>
-    </div>
+            <div class="lg:hidden absolute left-0 z-10 mt-2 w-56 rounded-md border border-gray-100 bg-white shadow-lg" role="menu" x-cloak x-transition x-show="isActive" x-on:click.away="isActive = false" x-on:keydown.escape.window="isActive = false">
+              <div class="p-2 mobile-menu">
+                <?php
+                wp_nav_menu(
+                  array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'primary-menu',
+                  )
+                );
+                ?>
+              </div>
+            </div>
+          </div>
+          <!-- /Mobile dropdown -->
+          <span class="sr-only">Logo</span>
+          <span class="inline-block w-20">
+            <?php the_custom_logo(); ?>
+          </span>
+        </div>
 
-    <div class="flex flex-1 items-center justify-end">
-      <nav
-        aria-label="Site Nav"
-        class="navigation-main hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500"
-      >
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			)
-		);
-		?>
-	  </nav>
+        <div class="flex flex-1 items-center justify-end">
+          <nav aria-label="Site Nav" class="navigation-main hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500">
+            <?php
+            wp_nav_menu(
+              array(
+                'theme_location' => 'menu-1',
+                'menu_id'        => 'primary-menu',
+              )
+            );
+            ?>
+          </nav>
 
-      <div class="ml-8 flex items-center">
-        <div
-          class="flex items-center divide-x divide-gray-100 border-x border-gray-100"
-        >
+          <div class="ml-8 flex items-center">
+            <div class="flex items-center divide-x divide-gray-100 border-x border-gray-100">
 
-		  <span>
-			<a
-              href="<?php echo wc_get_cart_url(); ?>"
-              class="flex items-center border-b-4 border-transparent p-6 hover:border-red-700"
-              title="<?php _e( 'View your shopping cart' ); ?>"
-            >
-              <svg
-                class="h-4 w-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                />
-              </svg>
-			<?php echo  WC()->cart->get_cart_contents_count(); ?>
-            </a>
-		</span>
+              <span>
+                <a href="<?php echo wc_get_cart_url(); ?>" class="flex items-center border-b-4 border-transparent p-4 hover:border-red-700 hover:text-red-700 hover:border-red-600" title="<?php _e('View your shopping cart'); ?>">
+                  <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                  </svg>
+                  <?php echo  WC()->cart->get_cart_contents_count(); ?>
+                </a>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</header>
-
+    </header>
